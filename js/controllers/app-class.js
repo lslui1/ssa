0e9d01509672b3b2e24c7f416dae3db98bdd749b
Peter <peterchoi1123@gmail.com>
2016-10-24 16:49:47 -0400

    Updated Add class with michael's code

--- js/controllers/app-class.js
+++ js/controllers/app-class.js
@@ -17,6 +17,10 @@ self.PROFlname = sessionStorage.PROFESSORlname
 
 self.useAlternativeClassId = sessionStorage.alternativeClassId
 
+self.showProfessorTable = false;
+
+
+
 
 self.subjectarray = [];
 
@@ -158,6 +162,103 @@ self.deletesavedclass = function(savedclassid) {
                         },function(err) {
 
                         })};
+
+
+  self.populate = function(firstName, lastName) {
+          document.getElementById("professor_fname").value = firstName;
+          document.getElementById("professor_lname").value = lastName;
+          document.getElementById("professor_fname").style.backgroundColor = 'lightgreen';
+          document.getElementById("professor_lname").style.backgroundColor = 'lightgreen';
+  }
+
+  self.enableInput = function() {
+        document.getElementById('professor_name').disabled = false;
+        self.showProfessorTable = true;
+  $http.get("http://localhost:8080/professors")
+      .then(function(resp) {
+        self.professors = resp.data;
+
+        for (professor of self.professors) {
+          professor.fullName = professor.first_name + " " + professor.last_name;
+        }
+        })
+  	}
+
+    self.addClassOrProf = function() {
+          var fname = document.getElementById("professor_fname").value;
+          var lname = document.getElementById("professor_lname").value;
+          var url = "http://localhost:8080/professorbyfnamelname/" + fname + "/" + lname;
+          var professorId = 0 ;
+                $.ajax({
+                url: url,
+                type: "GET"
+        }).then(function(resp){
+          if (resp.length == 0) {
+              var newProfessor = {};
+              newProfessor.university_id = 1;
+              newProfessor.first_name = fname;
+              newProfessor.last_name = lname;
+              $.ajax({
+                headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+                url: "http://localhost:8080/addprofessor",
+                type: "POST",
+                 data: JSON.stringify(newProfessor),
+                 dataType: 'text'
+             }).then(function() {
+              var url = "http://localhost:8080/professorbyfnamelname/" + fname + "/" + lname;
+                $.ajax({
+                url: url,
+                type: "GET"
+                }).then(function(resp){
+                      professorId = resp[0].id;
+                      addFunction(professorId)})
+            }
+          )
+    }
+          else {
+            var professorId = resp[0].id;
+            addFunction(professorId);
+          }
+      })}
+
+
+      function addFunction(professorId) {
+  var newClass = {};
+  var subject = document.getElementById("subject").value;
+  var section = document.getElementById("section").value;
+  newClass.university_id = 1;
+  newClass.professor_id = professorId;
+  newClass.name = subject;
+  newClass.section = section;
+  console.log(newClass)
+  $.ajax({
+    headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+    url: "http://localhost:8080/addclass",
+    type: "POST",
+    data: JSON.stringify(newClass),
+    dataType: 'text'
+}).then(function() {
+  console.log("hi")
+    $window.location.href = '/#/submittedclass';
+})
+}
+
+
+
+self.cantFindProfessor = function() {
+         document.getElementById("professor_fname").disabled = false;
+         document.getElementById("professor_lname").disabled = false;
+        document.getElementById("professor_fname").style.backgroundColor = '#b8d1f3';
+        document.getElementById("professor_lname").style.backgroundColor = '#b8d1f3';
+}
+
+
 };
 
 
